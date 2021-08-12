49efa035e443f44ba495897f1e43544cd007e615
guluke69 <gyulabodor95@gmail.com>
2021-08-12 08:06:45 +0200

    End of section 11

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -30,13 +30,21 @@ getMember(username: string){
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
-  updateMember(member: Member){
-    return this.http.put(this.baseUrl + 'users',member).pipe(
-      map(() => {
-        const index= this.members.indexOf(member);
-        this.members[index]=member;
-      })
-    )
-  }
+updateMember(member: Member){
+  return this.http.put(this.baseUrl + 'users',member).pipe(
+    map(() => {
+      const index= this.members.indexOf(member);
+      this.members[index]=member;
+    })
+  )
+}
+
+setMainPhoto(photoId: number){
+  return this.http.put(this.baseUrl + 'users/set-main-photo/'+ photoId,{});
+}
+
+deletePhoto(photoId: number){
+  return this.http.delete(this.baseUrl + 'users/delete-photo/' + photoId);
+}
 
 }
